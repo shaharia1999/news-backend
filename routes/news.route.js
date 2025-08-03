fbd35781683e0e03872ac36c2ee8215a96220f63
shaharia <adeptshaharia@gmail.com>
2025-08-03 23:01:18 +0600

    push

--- routes/news.route.js
+++ routes/news.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const newsController = require("../controllers/news.controller");
 const { visitorCounter } = require("../middleware/visitorCounter");
 router.post("/", newsController.createNews);
-router.get("/", newsController.getAllNews);
+router.get("/",visitorCounter, newsController.getAllNews);
 router.get('/data/top-views', newsController.getTotalViewsPerCategory);
 router.get('/:slug', visitorCounter, newsController.getNewsBySlug);
 // API for total views per category (no param)
