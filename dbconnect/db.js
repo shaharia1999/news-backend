2e73a25c1ed49488faf37d72042f68b4db9de825
shaharia <adeptshaharia@gmail.com>
2025-08-04 21:42:35 +0600

    push

--- dbconnect/db.js
+++ dbconnect/db.js
@@ -1,41 +1,5 @@
 
-
-// adeptshaharia
-// C9DWzWZzkBwSpU80
-// const mongoose = require("mongoose");
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect("mongodb://127.0.0.1:27017/testProductDb");
-//     console.log("MongoDB connected successfully");
-
-//     const collections = await mongoose.connection.db.listCollections().toArray();
-//     const hasnewsCollection = collections.some(col => col.name === 'news');
-
-//     if (hasnewsCollection) {
-//       const indexes = await mongoose.connection.collection('news').indexes();
-//       const hasNameIndex = indexes.find(index => index.name === 'name_1');
-
-//       if (hasNameIndex) {
-//         await mongoose.connection.collection('news').dropIndex('name_1');
-//         console.log("Dropped index: name_1");
-//       } else {
-//         console.log("No index named 'name_1' found.");
-//       }
-//     } else {
-//       console.log("Collection 'news' does not exist.");
-//     }
-
-//   } catch (err) {
-//     console.error("MongoDB connection failed:", err.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-// mongodb+srv://adeptshaharia:yGktw1WU7gunNpyn@cluster0.uol7ajw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 const mongoose = require("mongoose");
-// Replace <db_password> with your actual MongoDB Atlas password.
-// Ensure your IP address is whitelisted in MongoDB Atlas.
 const uri = "mongodb+srv://adeptshaharia:yGktw1WU7gunNpyn@cluster0.uol7ajw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const connectDB = async () => {
   try {
