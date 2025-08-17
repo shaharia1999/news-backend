389988ba206fff98ed6f41fbb45deb52f270fcc1
shaharia <adeptshaharia@gmail.com>
2025-08-17 11:18:08 +0600

    news

--- routes/news.route.js
+++ routes/news.route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const newsController = require("../controllers/news.controller");
 const { visitorCounter } = require("../middleware/visitorCounter");
+const verifyToken = require("../middleware/auth.middleware");
+const RoleCheck = require("../middleware/RoleCheck");
 router.post("/",verifyToken, RoleCheck(['moderator', 'admin']), newsController.createNews);
 router.get("/", newsController.getAllNews);
 router.get('/data/top-views', newsController.getTotalViewsPerCategory);
